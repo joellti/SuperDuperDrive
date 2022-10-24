98c05abc3c5987b12c77955f90bacc65ddfd0719
Joe <joellti@outlook.com>
2022-10-24 12:13:24 -0700

    Support war

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplication.java
@@ -2,10 +2,16 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class CloudStorageApplication extends SpringBootServletInitializer  {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CloudStorageApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudStorageApplication.class, args);
